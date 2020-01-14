f7af648422040382317ea4c6d0fa0e82413fd5c6
timon <timon0596@yandex.ru>
2020-01-15 02:03:18 +0300

    collection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
-import './style.sass'
 import 'bootstrap'
 import 'bootstrap/scss/bootstrap.scss'
+import './style.sass'
 import './index.pug'
 require.context("./img", true, /\.svg$/)
+
+
+$('#carouselExampleIndicators').carousel({interval: false})
